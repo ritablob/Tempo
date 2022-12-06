1106304d17b35a53b9496de6c6cedf7f14bbe289
xXLordXx-CoolDood <langlandsx@gmail.com>
2022-12-06 14:11:13 +0100

    Camera follows in a 2D way

--- Assets/Scripts/DynamicCamera.cs
+++ Assets/Scripts/DynamicCamera.cs
@@ -5,31 +5,31 @@ using UnityEngine;
 public class DynamicCamera : MonoBehaviour
 {
     [Header("Objects to Follow")]
-    [SerializeField] Transform obj1;
-    [SerializeField] Transform obj2;
+    [SerializeField] Transform player1;
+    [SerializeField] Transform player2;
 
-    [Header("Camera Parameters")]
-    [SerializeField] float maximumZoom;
-    [SerializeField] float minimumZoom;
-    [SerializeField] float cameraSpeed; //How fast the camera moves to follow the players
-    [SerializeField] float zoomPower; //How much the camera zooms in/out depending on the distance between the players
 
-    void Update()
+    private void LateUpdate()
     {
-        Vector3 middlePoint = obj1.position + (obj2.position - obj1.position) / 2; //Get middle point to focus on
-        transform.LookAt(middlePoint, Vector3.up); //Look at middle point between the two targets
+        Vector3 centerPoint = GetCenterPoint();
 
+        Vector3 direction = player1.position - player2.position;
+        Vector3 newDirection = Vector3.Cross(direction, Vector3.up).normalized;
 
-        float distance = Vector3.Distance(obj1.position, obj2.position); //Get distance between two targets, calculate zoom of camera
-        distance *= zoomPower;
-        distance = Mathf.Clamp(distance, minimumZoom, maximumZoom);
+        Vector3 newPosition = centerPoint + (newDirection * 5);
+        newPosition.y = 5;
 
-        Vector3 newPosition = new Vector3(middlePoint.x * distance, gameObject.transform.position.y, middlePoint.z * distance); //Find new location for camera
+        gameObject.transform.position = newPosition;
 
-        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, newPosition, Time.deltaTime * cameraSpeed);
+        Debug.DrawLine(centerPoint, newPosition, Color.green);
+        Debug.DrawLine(centerPoint, gameObject.transform.position, Color.red);
+        gameObject.transform.LookAt(centerPoint);
+    }
 
-        //Debugging
-        Debug.Log(distance);
-        Debug.DrawLine(gameObject.transform.position, middlePoint);
+    Vector3 GetCenterPoint()
+    {
+        Vector3 center = Vector3.Lerp(player1.position, player2.position, 0.5f);
+
+        return center;
     }
 }
