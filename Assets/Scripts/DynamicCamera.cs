6fa5f59775c7eb7bb3c5087b3ad388e00909d365
xXLordXx-CoolDood <langlandsx@gmail.com>
2022-12-07 14:58:34 +0100

    Smooth, sexy, lubricated camera now worls

--- Assets/Scripts/DynamicCamera.cs
+++ Assets/Scripts/DynamicCamera.cs
@@ -8,22 +8,63 @@ public class DynamicCamera : MonoBehaviour
     [SerializeField] Transform player1;
     [SerializeField] Transform player2;
 
+    [Header("Camera Parameters")]
+    [SerializeField] float maxBlendDistance;
+    [SerializeField] float cameraZoomGrowth;
+    [SerializeField] float cameraHeightGrowth;
+    [SerializeField] float cameraSpeed;
+    [SerializeField] float cameraRotateSpeed;
+
+    private Vector3 velocity = Vector3.zero;
 
     private void LateUpdate()
     {
+        //Calculate & set the close position for the camera
         Vector3 centerPoint = GetCenterPoint();
-
         Vector3 direction = player1.position - player2.position;
         Vector3 newDirection = Vector3.Cross(direction, Vector3.up).normalized;
 
         Vector3 newPosition = centerPoint + (newDirection * 5);
-        newPosition.y = 5;
+        newPosition.y = 3;
 
-        gameObject.transform.position = newPosition;
+        //Calculate & set the wide position for the camera
+        float distance = Vector3.Distance(player1.position, player2.position);
 
-        Debug.DrawLine(centerPoint, newPosition, Color.green);
-        Debug.DrawLine(centerPoint, gameObject.transform.position, Color.red);
-        gameObject.transform.LookAt(centerPoint);
+        if(distance > 8) //If distance is less than 8, start blending positions
+        {
+            Vector3 directionToCenter = gameObject.transform.position - centerPoint; //Get axis to zoom out on, normalize it
+            directionToCenter.Normalize();
+            distance = Mathf.Clamp(distance * cameraZoomGrowth, 5, 99);
+            Vector3 zoomPosition = centerPoint + (directionToCenter * distance);
+            zoomPosition.y = Mathf.Clamp(distance * cameraHeightGrowth, 3, 99); //Get height of camera
+
+            Vector3 blendedVector = GetBlendedVector(distance, newPosition, zoomPosition);
+
+            gameObject.transform.position = Vector3.Slerp(gameObject.transform.position, blendedVector, cameraSpeed * Time.deltaTime);
+
+            Debug.DrawRay(centerPoint, directionToCenter, Color.blue);
+            Debug.DrawLine(centerPoint, zoomPosition, Color.green);
+        }
+        else //If the camera is close to the objects, get a close perspective.
+        {
+            gameObject.transform.position = Vector3.Slerp(gameObject.transform.position, newPosition, cameraSpeed * Time.deltaTime);
+        }
+
+        Vector3 lookDirection = centerPoint - gameObject.transform.position;
+        lookDirection.Normalize();
+
+        gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, Quaternion.LookRotation(lookDirection), cameraRotateSpeed * Time.deltaTime);
+
+        Debug.DrawLine(player1.position, player2.position);
+    }
+
+    Vector3 GetBlendedVector(float distance, Vector3 pos1, Vector3 pos2)
+    {
+        float blendAmount = Mathf.Clamp(distance / maxBlendDistance, 0, 1);
+
+        Vector3 blendedVector = Vector3.Lerp(pos1, pos2, blendAmount);
+
+        return blendedVector;
     }
 
     Vector3 GetCenterPoint()
