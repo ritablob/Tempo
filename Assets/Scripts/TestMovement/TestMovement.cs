4d6d58cd0a2ecbc8a28463326a48342642171ef1
xXLordXx-CoolDood <langlandsx@gmail.com>
2022-12-09 22:05:34 +0100

    Basic controller support. Updated player input settings

--- Assets/Scripts/TestMovement/TestMovement.cs
+++ Assets/Scripts/TestMovement/TestMovement.cs
@@ -3,47 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInput))]
 public class TestMovement : MonoBehaviour
 {
+    [SerializeField] GameObject attackIndicator;
+
     public float speed = 1f;
-    private Rigidbody rb;
-    private Vector3 move;
-    // Start is called before the first frame update
-    void Start()
+    public CharacterController charController;
+    public float controllerDeadZone = 0.1f;
+    public float gamePadSmoothingRate = 1000f;
+
+    private Vector2 movement;
+    private Vector3 aim;
+    private Vector3 playerVelocity;
+
+
+    private PlayerControls playerControls;
+    private PlayerInput playerInput;
+
+    void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        playerControls = new PlayerControls();
+        charController = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void OnEnable()
     {
-        if (Keyboard.current.anyKey.isPressed)
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    void Update()
+    {
+        HandleInput();
+        HandleMovement();
+        HandleRotation();
+    }
+
+    void HandleInput()
+    {
+        movement = playerControls.Player.Move.ReadValue<Vector2>();
+        aim = playerControls.Player.Look.ReadValue<Vector2>();
+    }
+
+    void HandleMovement()
+    {
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
+        charController.Move(move * Time.deltaTime * speed);
+    }
+
+    void HandleRotation()
+    {
+        if (Mathf.Abs(aim.x) > controllerDeadZone || Mathf.Abs(aim.y) > controllerDeadZone)
         {
-            if (Keyboard.current.wKey.isPressed)
+            Vector3 playerDirection = Vector3.right * aim.x + Vector3.forward * aim.y;
+
+            if(playerDirection.sqrMagnitude > 0.0f)
             {
-                move = Vector3.forward; // 1,0
+                Quaternion newRotation = Quaternion.LookRotation(playerDirection, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, newRotation, gamePadSmoothingRate * Time.deltaTime);
             }
-            else if (Keyboard.current.sKey.isPressed)
-            {
-                move = Vector3.back; // -1,0
-            }
-            else if (Keyboard.current.aKey.isPressed)
-            {
-                move = Vector3.left;
-            }
-            else if (Keyboard.current.dKey.isPressed)
-            {
-                move = Vector3.right;
-            }
-            rb.AddForce(move * speed, ForceMode.VelocityChange);
         }
-
-        {
-            move = Vector3.zero;
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
-        }
-
-
     }
 }
