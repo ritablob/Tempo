4293887d8c69d05e7c424a37e583b8d3ee82e038
ritablob <106591191+ritablob@users.noreply.github.com>
2023-01-25 15:45:48 +0100

    menus and settings and stuff, pt 2

--- Assets/Scripts/SoundEffectsMenu.cs
+++ Assets/Scripts/SoundEffectsMenu.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundEffectsMenu : MonoBehaviour
 {
     public List<Sound> menuSounds;
     private static SoundEffectsMenu instance;
     public float soundDestroyDelay = 0.3f;
+    public AudioMixerGroup audioMixer;
     public static SoundEffectsMenu Instance
     {
         get { return instance; }
