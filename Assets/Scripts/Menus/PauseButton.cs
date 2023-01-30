bae4bfab3f8ca1e7f1f4efce1fb98cdb70a4dfba
ritablob <106591191+ritablob@users.noreply.github.com>
2023-01-30 19:36:15 +0100

    a bit of UI work

--- Assets/Scripts/Menus/PauseButton.cs
+++ Assets/Scripts/Menus/PauseButton.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PauseButton : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
     PauseMenu pauseMenu;
+    public Sprite clickedPointer;
+    private Pointer pointer;
+
     void Start()
     {
         pauseMenu = FindObjectOfType<PauseMenu>();
+    }
+    public void PointerSpriteSwitch()
+    {
+        pointer = gameObject.GetComponentInChildren<Pointer>();
+        //Debug.Log("pointer found " + pointer.name);
+        pointer.gameObject.GetComponent<Image>().sprite = clickedPointer;
     }
     public void OnDeselect(BaseEventData eventData)
     {
@@ -21,5 +31,6 @@ public class PauseButton : MonoBehaviour, ISelectHandler, IDeselectHandler
     public void OnSelect(BaseEventData eventData)
     {
         Instantiate(pauseMenu.pointer.gameObject, gameObject.transform);
+
     }
 }
--- Assets/Scripts/PlayerUIController.cs
+++ Assets/Scripts/PlayerUIController.cs
@@ -15,6 +15,8 @@ public class PlayerUIController : MonoBehaviour
     [Header("0 - PoleDancer, 1 - BreakDancer")]
     public List<Sprite> portraitImages;
     public GameObject slider2Parent;
+    public Image ultimateGlow;
+    public float glowExpansionOnFullUltimate = 1.5f;
 
     private DynamicCamera camera;
     private Transform[] slider2Children;
@@ -23,6 +25,7 @@ public class PlayerUIController : MonoBehaviour
     private PlayerMovement playerMovement;
     private float ultimateCharge;
     private float onePiece;
+    float alpha;
     private void Start()
     {
         camera = new DynamicCamera();
@@ -94,6 +97,16 @@ public class PlayerUIController : MonoBehaviour
         if (barCount < 0)
         {
             barCount = 0;
+        }
+        alpha = onePiece * barCount;
+        ultimateGlow.color = new Color(ultimateGlow.color.r, ultimateGlow.color.g, ultimateGlow.color.b, alpha);
+        if (barCount >= 24)
+        {
+            ultimateGlow.transform.localScale = new Vector3(1f, glowExpansionOnFullUltimate, 1f);
+        }
+        else
+        {
+            ultimateGlow.transform.localScale = new Vector3(1f, 1f, 1f);
         }
         //Debug.Log(pMovement.ultimateCharge+" "+pMovement.ultimateLimit + "Bar count for " + pMovement.gameObject.name + " " + barCount);
         for (int i = 0; i < slider2Children.Length; i++)
